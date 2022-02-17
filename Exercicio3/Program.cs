3a2980962fc7abb400085b90daebfc054e030b5b
Stawerr <Renato.Rodrigues.T0121547@edu.atec.pt>
2022-02-17 16:30:43 +0000

    last comm

---
+++ Exercicio3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+namespace Exercicio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader rd = new StreamReader("C:\\produtos.csv");
+          //  StreamWriter wd = new StreamWriter("C:\\produtos.csv");
+
+            while (!rd.EndOfStream)
+            {
+                string linha = rd.ReadLine();
+                string[] palavras = linha.Split();
+               
+                    Console.WriteLine(palavras[4]);
+            }
+            rd.Close();
+            //wd.Close();
+            
+        }
+    }
+}
