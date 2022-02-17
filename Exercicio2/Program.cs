53832d11760eba9df5b3a1dd27c17c9cb5c410e1
Marta <Marta@DESKTOP-5QBNVEI>
2022-02-17 14:27:32 +0000

    Adicionar arquivos de projeto.

---
+++ Exercicio2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+namespace Exercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader rd = new StreamReader("C:\\notas.txt");
+            StreamWriter wd = new StreamWriter("C:\\aprovado.txt");
+            StreamWriter wd2 = new StreamWriter("C:\\reprovado.txt");
+
+            while (!rd.EndOfStream)
+            {
+                string linha = rd.ReadLine();
+                string[] palavras = linha.Split(' ');
+                
+                if (double.Parse(palavras[2]) >= 9.5){
+                    wd.WriteLine(linha);
+                }
+                else
+                {
+                    wd2.WriteLine(linha);
+                }
+            }
+            rd.Close();
+            wd.Close();
+            wd2.Close();
+        }
+    }
+}
---
+++ LeituraeEscritaFiles/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+namespace LeituraeEscritaFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader rd = new StreamReader("C:\\vencimentos.txt");
+            StreamWriter wd = new StreamWriter("C:\\SUPMIL.txt");
+
+            while (!rd.EndOfStream)
+            {
+                string linha = rd.ReadLine();
+                string[] palavras = linha.Split(' ');
+
+                if (int.Parse(palavras[2]) > 1000)
+                {
+                    wd.WriteLine(linha);
+                }
+            }
+            rd.Close();
+            wd.Close();
+        }
+    }
+}
